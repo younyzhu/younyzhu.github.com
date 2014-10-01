672a396ed45115703023b0c6fec9ab718ccb7217
Yongnan Zhu <younyzhu@gmail.com>
2014-10-01 11:53:35 -0400

    init

--- PathBubblesWeb/js/PathBubbles.js
+++ PathBubblesWeb/js/PathBubbles.js
@@ -4,15 +4,11 @@
  * @time        9/16/2014
  * @name        PathBubble
  */
-var PATHBUBBLES = PATHBUBBLES || {};
-
-(function(){
-    PATHBUBBLES = {
+var PATHBUBBLES = {
         // public
         REVISION: '0.0',
         selectionBoxSize: 6,
         objects: []
-    }
-})();
+    };
 
 
